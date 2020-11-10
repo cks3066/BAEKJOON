c501e17af94be0c49f9db58997db17752a93160d
cks3066 <62434898+cks3066@users.noreply.github.com>
2020-11-10 16:58:52 +0900

    Create 09.c

---
+++ 03 for문/09 별 찍기1/09.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int A;
+	scanf("%d", &A);
+	
+	for (int i = 0; i < A; i++)
+	{
+		for (int j = 0; j < i+1; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
