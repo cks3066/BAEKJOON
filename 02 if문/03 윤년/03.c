1fcd1b0f7c0ef697c4c24a0a6156f873dd28cc21
cks3066 <62434898+cks3066@users.noreply.github.com>
2020-11-10 16:52:04 +0900

    Create 03.c

---
+++ 02 if문/03 윤년/03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int A;
+
+	scanf("%d", &A);
+
+	if (A % 4 == 0 && A % 100 != 0)
+		printf("%d", 1);
+	else if (A % 400 == 0)
+		printf("%d", 1);
+	else
+		printf("%d", 0);
+
+	return 0;
+}
