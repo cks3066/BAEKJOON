e9c0ca9707598520b45a514624b0d40f4aed407c
cks3066 <62434898+cks3066@users.noreply.github.com>
2020-11-10 16:51:37 +0900

    Create 02.py

---
+++ 02 if문/02 시험 성적/02.py
@@ -0,0 +1,12 @@
+a = input()
+
+if int(a) in range(90,101):
+    print('A')
+elif int(a) in range(80,90):
+    print('B')
+elif int(a) in range(70,80):
+    print('C')
+elif int(a) in range(60,70):
+    print('D')
+else:
+    print('F')
