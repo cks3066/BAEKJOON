6b4fdd3f669996d373db1b70ccad8b76a8148459
On <62434898+cks3066@users.noreply.github.com>
2021-01-05 22:28:59 +0900

    Create 04.cpp

---
+++ 13 백트래킹/04 N과 M (4)/04.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX 8+1
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int lists[MAX];
+bool visited[MAX] = { 0, };
+
+void BackTracking(int cnt, int N, int M) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << lists[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (cnt == 0) {
+            if (!visited[i]) {
+                lists[cnt] = i;
+                BackTracking(cnt + 1, N, M);
+            }
+        }
+        else {
+            if (!visited[i] && i >= lists[cnt-1]) {
+                lists[cnt] = i;
+                BackTracking(cnt + 1, N, M);
+            }
+
+        }
+
+    }
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+
+	int N,M;
+
+	cin >> N >> M;
+
+    BackTracking(0,N, M);
+
+	return 0;
+}
