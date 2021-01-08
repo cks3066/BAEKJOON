000bb19b9a2380b559f6f3240f110f4d42b581ed
On <62434898+cks3066@users.noreply.github.com>
2021-01-08 14:14:12 +0900

    Create 03.cpp

---
+++ 14 동적 계획법 1/01타일/03.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX 1000000+1
+#include <iostream>
+
+using namespace std;
+
+int memo[MAX];
+bool filled[MAX];
+
+int AbleTile(int N) {
+	if (N <= 2) return N;
+	if (filled[N] == true)
+		return memo[N] % 15746;
+
+	memo[N] = (AbleTile(N - 1) + AbleTile(N - 2)) % 15746;
+	filled[N] = true;
+	return memo[N];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+
+	int N;
+
+	for (int i = 0; i < 3; i++) {
+		memo[i] = i;
+		filled[i] = true;
+	}
+
+	cin >> N;
+
+	cout << AbleTile(N);
+
+	return 0;
+}
