c78770a636134472a05b57b3a0cba52af191940b
On <62434898+cks3066@users.noreply.github.com>
2021-01-17 23:31:22 +0900

    Create 01.cpp

---
+++ 18 큐,덱/01 큐2/01.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <queue>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    queue<int> s;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        string cmd;
+        cin >> cmd;
+        if (cmd == "push") {
+            int num;
+            cin >> num;
+            s.push(num);
+        }
+        else if (cmd == "front") {
+            cout << (s.empty() ? -1 : s.front()) << '\n';
+        }
+        else if (cmd == "size") {
+            cout << (s.empty() ? 0 : s.size()) << '\n';
+        }
+        else if (cmd == "pop") {
+            cout << (s.empty() ? -1 : s.front()) << '\n';
+            if (!s.empty()) s.pop();
+        }
+        else if (cmd == "empty") {
+            cout << (s.empty() ? 1 : 0) << '\n';
+        }
+        else if (cmd == "back") {
+            cout << (s.empty() ? -1 : s.back()) << '\n';
+        }
+    }
+
+    return 0;
+}
