d3e12a1f3359ef97fecb0cbfcee5ca6113893ee1
On <62434898+cks3066@users.noreply.github.com>
2021-01-18 03:33:46 +0900

    Create 03.cpp

---
+++ 18 큐,덱/03 요세푸스 문제 0/03.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX 1000+1
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int N,K;
+vector<int> v;
+bool visited[MAX];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> K;
+
+    int index = K;
+    int cnt = 0;
+    for (int i = 0; i < N; i++) {
+        cnt = 0;
+        v.push_back(index);
+        visited[index] = true;
+        if (i != N-1) {
+            while (cnt != K) {
+                index++;
+                if (index > N) index -= N;
+                if (visited[index] == false)
+                    cnt++;
+            }
+        }
+    }
+
+    cout << "<";
+    for (int i = 0; i < N - 1; i++) {
+        cout << v[i] << ", ";
+   }
+    cout << v[N - 1] << ">";
+
+    return 0;
+}
