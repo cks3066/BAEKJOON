4bc9c7c50599b291b3e036986be91bc4c9e411b9
On <62434898+cks3066@users.noreply.github.com>
2021-02-02 17:14:16 +0900

    Create 11722.cpp

---
+++ 00 ETC/가장 긴 감소하는 부분 수열/11722.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int N;
+int memo[1000 + 1];
+int arr[1000 + 1];
+
+void DP(int n) {
+    if (n == N) memo[n] = 1;
+    else {
+        int gt = 0;
+        for (int i = n + 1; i <= N; i++) {
+            if (arr[n] > arr[i] && gt < memo[i])
+                gt = memo[i];
+        }
+        memo[n] = gt + 1;
+    }
+    if (n == 1) return;
+    DP(n - 1);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+
+    DP(N);
+
+    int gt = 0;
+    for (int i = 1; i <= N; i++) {
+        if (memo[i] > gt) gt = memo[i];
+    }
+
+    cout << gt;
+
+    return 0;
+}
